c41a67b2e310d91046e63c9949ede762f0a38b2f
kentanvictor <2030457926@qq.com>
2017-05-13 17:01:44 +0800

    查看联系人列表(注解)

--- app/src/main/java/com/example/dell/contactstest/MainActivity.java
+++ app/src/main/java/com/example/dell/contactstest/MainActivity.java
@@ -22,7 +22,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ListView contactsView = (ListView) findViewById(R.id.contacts_view);
+        ListView contactsView = (ListView) findViewById(R.id.contacts_view);//获取控件实例
+        //下面是给ListView配置适配器
         adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,contactsList);
         contactsView.setAdapter(adapter);
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED)
@@ -34,16 +35,20 @@ public class MainActivity extends AppCompatActivity {
             readContacts();
         }
     }
-
+//调用readContacts()方法读取系统联系人信息
     private void readContacts() {
         Cursor cursor = null;
         try
         {
             //查询联系人数据
             cursor = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,null,null,null,null);
+            //ContactsContract.CommomDataKinds.Phone类已经做好了封装
+            //提供了一个CONTENT_URI的常量
+            //这个常量就是使用Uri.parse()方法去解析出来的结果
             if(cursor != null)
             {
                 while(cursor.moveToNext())
+                    //对Cursor对象进行遍历，将联系人和电话进行遍历，
                 {
                     //获取联系人姓名
                     String displayName = cursor.getString(cursor.getColumnIndex
@@ -67,6 +72,8 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
+    //下面是一个关于是否授权的回调
+    //授权的结果都会封装在grantResults参数中。这里我们只需要判断一下最后的授权结果
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults)
     {
