c54475c55a8f14547e4f55c939846aa50eab3705
kentanvictor <2030457926@qq.com>
2017-05-13 16:01:18 +0800

    Initial commit

---
+++ app/src/main/java/com/example/dell/contactstest/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.dell.contactstest;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    ArrayAdapter<String> adapter;
+    List<String> contactsList = new ArrayList<>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ListView contactsView = (ListView) findViewById(R.id.contacts_view);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,contactsList);
+        contactsView.setAdapter(adapter);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.READ_CONTACTS} , 1);
+        }
+        else
+        {
+            readContacts();
+        }
+    }
+
+    private void readContacts() {
+        Cursor cursor = null;
+        try
+        {
+            //查询联系人数据
+        }catch (Exception e)
+        {
+            cursor.close();
+        }
+    }
+}
